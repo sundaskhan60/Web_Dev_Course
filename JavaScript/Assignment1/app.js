86d187590aaae11a1228d4beb42c6ebba0a4ab43
sundaskhan60 <sundaskhan0324@gmail.com>
2026-01-07 18:39:30 +0500

    Assigment 1 of js Alert q1 to 9

---
+++ JavaScript/Assignment1/app.js
@@ -0,0 +1,15 @@
+// Q1: Greet website visitor
+alert("Welcome to our website questionn1");
+
+// Q2: Password alert
+alert("Alert! Please enter a valid password question2");
+
+// Q3: Alert with new line
+alert("Welcome to JS Land...\nHappy Coding!   question3");
+
+// Q4: Display messages in sequence
+alert("Welcome to JS Land...  question4");
+alert("Prevent this page from creating additional dialogs");
+
+// Q5: Message through browser console
+console.log("Hello... I can run JS through my web browser's console question5");
